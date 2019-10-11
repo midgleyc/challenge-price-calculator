93b6550a72be44643c40e6acee9ff04f64997efd
Chris Midgley <midgleyc@gmail.com>
2019-10-11 17:37:13 +0100

    Finish off basket parser tests.

--- test/Library.PriceCalculator.Tests/BasketParserTests.cs
+++ test/Library.PriceCalculator.Tests/BasketParserTests.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Library.PriceCalculator.Contract;
 using Library.PriceCalculator.Parsing;
@@ -28,6 +28,37 @@ namespace Library.PriceCalculator.Tests
             present.Price.Should().Be(11.12m);
         }
 
+        [TestMethod]
+        public void InventoryNotPassedItemIsAbsent()
+        {
+            var basketParser = CreateBasketParser();
+            var output = basketParser.ParseBasket(new string[] {"Absent"});
+            output.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void ParseCanContainDuplicates()
+        {
+            var basketParser = CreateBasketParser();
+            var output = basketParser.ParseBasket(new string[] {"Present", "Present", "Present"});
+            output.Count.Should().Be(3);
+            var present = output.Last();
+            present.Identifier.Should().Be("Present");
+            present.Price.Should().Be(11.12m);
+        }
+
+        [TestMethod]
+        public void ParseContainsAsMuchAsPossible()
+        {
+            var basketParser = CreateBasketParser();
+            var output = basketParser.ParseBasket(new string[] {"Present", "Absent", "Present2", "Absent2"});
+            output.Count.Should().Be(2);
+            var present = output.Should().Contain(i => i.Identifier == "Present").Subject;
+            present.Price.Should().Be(11.12m);
+            var present2 = output.Should().Contain(i => i.Identifier == "Present2").Subject;
+            present2.Price.Should().Be(23.25m);
+        }
+
         private IInventory CreateInventory() {
             return new TestInventory();
         }
@@ -40,6 +71,8 @@ namespace Library.PriceCalculator.Tests
         {
             private readonly Dictionary<string, decimal> priceMapping = new Dictionary<string, decimal> {
                 {"Present", 11.12m},
+                {"Present2", 23.25m},
+                {"Present3", 37.40m},
             };
 
             public bool TryGetPriceFor(string itemName, out decimal price)
