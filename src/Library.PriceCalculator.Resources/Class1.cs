7d7c0a3542711501fc243b31f0242476cd94a448
Chris Midgley <midgleyc@gmail.com>
2019-10-11 15:53:33 +0100

    Add skeleton directory structure.

---
+++ src/Library.PriceCalculator.Resources/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Library.PriceCalculator.Resources
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Service.PriceCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service.PriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
